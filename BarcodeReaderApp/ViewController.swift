01746f09b004f90efc6e47d9c96589cbaab80f61
wataru0 <wataru.tv.nanao@gmail.com>
2020-09-07 21:09:17 +0900

    make UI

--- BarcodeReaderApp/ViewController.swift
+++ BarcodeReaderApp/ViewController.swift
@@ -23,7 +23,29 @@ class ViewController: UIViewController {
         
         // 読み込めるカメラ範囲
         barcodeReader.readRange()
+        
+        textLabel.backgroundColor = .black
     }
 
+}
+
+extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    // バーコードを認識，読み込んだ時に呼ばれる
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // 画面上に複数のバーコードがある場合，複数読み込むが，今回は便宜的に先頭のオブジェクトを処理
+        if let metadata = metadataObjects.first as? AVMetadataMachineReadableCodeObject {
+            let barCode = barcodeReader.previewLayer.transformedMetadataObject(for: metadata) as! AVMetadataMachineReadableCodeObject
+            //読み込んだQRを映像上で枠を囲む。ユーザへの通知。必要な時は記述しなくてよい。
+            barcodeReader.qrView.frame = barCode.bounds
+            //QRデータを表示
+            if let str = metadata.stringValue {
+                //print(str)
+                textLabel.text = str
+                
+                textLabel.textColor = .white
+            }
+        }
+    }
 }
 
