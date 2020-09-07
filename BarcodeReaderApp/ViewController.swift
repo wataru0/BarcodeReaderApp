e512ce4e87edebbb0c25ebb31e6e0ed73151bfa4
wataru0 <wataru.tv.nanao@gmail.com>
2020-09-07 20:46:16 +0900

    initial commit

---
+++ BarcodeReaderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeReaderApp
+//
+//  Created by 岡本航昇 on 2020/09/07.
+//  Copyright © 2020 wataru okamoto. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
